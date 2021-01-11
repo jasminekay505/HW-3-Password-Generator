2871e56e0e6f67c33fdc5646f18a5b9290198eae
Jasmine Kay <jasminekay505@gmail.com>
2021-01-10 17:22:57 -0700

    created empty array userchoices and prompts for password length and characters wanted

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var num = "0123456789"
-var symbol = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var symbol = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var userChoices = ""
 
 // Write password to the #password input
 function writePassword() {
@@ -11,7 +13,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
@@ -19,5 +21,40 @@ generateBtn.addEventListener("click", writePassword);
 
 //Create generatePassword Function
 function generatePassword() {
+  //Ask for user input
+  //Ask for password length
+  var passwordLength = promt("How many characters do you want in your new password? Choose between 8 and 128.");
+  //Validate input
+  if(!passwordLength) {
+    alert("You must choose a how many characters you want in your new password!");
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLenght = prompt = ("You must choose between 8 and 128.");
+  } else {
+    //Ask for password composition
+    var wantUpper = confirm("Do you want upper case characters in your password?");
+    //Add upper case characters into array
+    if (wantUpper) {
+      userChoices = userChoices + upperLetters;
+    }
+    var wantLower = confirm("Do you want lower case characters in your password?");
+    //Add upper case characters into array
+    if (wantLower) {
+      userChoices = userChoices + lowerLetters;
+    }
+    var wantNum = confirm("Do you want numbers in your password?")
+    //Add numbers into array
+    if(wantNum) {
+      userChoices = userChoices + num;
+    }
+    var wantSymbol = confirm("Do you want special characters in your password (!@#$% etc.");
+    //Add symbols into array
+    if (wantSymbol) {
+      userChoices = userChoices + symbol;
+    }
+  }
+
+  }
+ 
+ 
 
 }
