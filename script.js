b593c3e6acba4ff6d6c4ee9acbf56355de2e0798
Jasmine Kay <jasminekay505@gmail.com>
2021-01-10 18:59:43 -0700

    Filled out for loop so that it creates password and function returns password

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Create strings with character options
 var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var symbol = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var userChoices = ""
+//Create blank string to fill with user choices for characters
+var userChoices = "";
+//Create blank password to be filled up with characters
+var password = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -27,39 +31,47 @@ function generatePassword() {
   //Validate input
   if(!passwordLength) {
     alert("You must choose a how many characters you want in your new password!");
+  //Validate length input  
   } else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLenght = prompt = ("You must choose between 8 and 128.");
+    passwordLength = prompt("You must choose between 8 and 128.");
   } else {
     //Ask for password composition
+    //Ask if user wants Upper case letters
     var wantUpper = confirm("Do you want upper case characters in your password?");
-    //Add upper case characters into array
+    //Add upper case characters to userChoices
     if (wantUpper) {
       userChoices = userChoices + upperLetters;
     }
+    //Ask if user wants Lower case letters
     var wantLower = confirm("Do you want lower case characters in your password?");
-    //Add upper case characters into array
+    //Add upper case characters to userChoices
     if (wantLower) {
       userChoices = userChoices + lowerLetters;
     }
+    //Ask if user wants numbers
     var wantNum = confirm("Do you want numbers in your password?")
-    //Add numbers into array
+    //Add numbers to userChoices
     if(wantNum) {
       userChoices = userChoices + num;
     }
+    //Ask if user wants symbols
     var wantSymbol = confirm("Do you want special characters in your password (!@#$% etc.");
-    //Add symbols into array
+    //Add symbols to userChoices
     if (wantSymbol) {
       userChoices = userChoices + symbol;
     }
+    //Validate input, if user chooses no characters
     if(!wantUpper && !wantLower && !wantNum && !wantSymbol) {
-      userChoices = alert("You must choose which characters you want in your password!");
+      userChoices = alert("You must choose at least one character for your password!");
     }
 
     //For loop to create randomly generated password
     for (var i = 0; i < passwordLength; i ++) {
-      
+      var character = userChoices[Math.floor(Math.random() * userChoices.length)];
+      password += character;
     }
   }
-
+  //Function returns password to be printed in writePassword function 
+  return password;
   }
  
